5772c9fb96555d8bf5bfa7b3e55964a9ffe16937
Aarni Koskela <akx@iki.fi>
2021-04-07 14:38:18 +0300

    Add actual detection code

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,49 @@ function loadImage(file: File): Promise<InputImage> {
   });
 }
 
+function computeChangeRows(imageData: ImageData): [number, number][] {
+  const rowLen = imageData.width * 4;
+  const changeRows: [number, number][] = [];
+  let lastChangeY = 0;
+  for (let y = 1; y < imageData.height; y++) {
+    const lastOffset = (y - 1) * rowLen;
+    const thisOffset = y * rowLen;
+    const nextOffset = (y + 1) * rowLen;
+    const lastRow = imageData.data.subarray(lastOffset, thisOffset - 1);
+    const row = imageData.data.subarray(thisOffset, nextOffset - 1);
+    if (!row.every((v, i) => v === lastRow[i])) {
+      changeRows.push([lastChangeY, y]);
+      lastChangeY = y;
+    }
+  }
+  return changeRows;
+}
+
+function getCounts<T>(arr: readonly T[]): [Map<T, number>, number] {
+  const counts: Map<T, number> = new Map();
+  let highestCount = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const key = arr[i];
+    const count = (counts.get(key) || 0) + 1;
+    counts.set(key, count);
+    highestCount = count > highestCount ? count : highestCount;
+  }
+  return [counts, highestCount];
+}
+
+function mostCommon<T>(arr: readonly T[]): [T | undefined, number] {
+  const [counts, highestCount] = getCounts(arr);
+  for (const [key, count] of counts) {
+    if (count === highestCount) return [key, count];
+  }
+  return [undefined, 0];
+}
+
 function App() {
   const [input, setInput] = React.useState<InputImage | null>();
-  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const [scaleFactor, setScaleFactor] = React.useState<number>(1);
+  const originalCanvasRef = React.useRef<HTMLCanvasElement>(null);
+  const scaledCanvasRef = React.useRef<HTMLCanvasElement>(null);
 
   const handleImageChange = React.useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,26 +79,62 @@ function App() {
     []
   );
   React.useEffect(() => {
-    let canvas = canvasRef.current;
+    const canvas = originalCanvasRef.current;
     if (!(input && canvas)) {
       return;
     }
-    canvas.width = input.image.width;
-    canvas.height = input.image.height;
+    const width = (canvas.width = input.image.width);
+    const height = (canvas.height = input.image.height);
     const ctx = canvas.getContext("2d");
     if (!ctx) {
       return;
     }
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, width, height);
     ctx.drawImage(input.image, 0, 0);
-  }, [canvasRef.current, input]);
+    const changeRows = computeChangeRows(ctx.getImageData(0, 0, width, height));
+    changeRows.forEach(([y0, y1], i) => {
+      ctx.fillStyle = "red";
+      ctx.fillRect(width / 2 + (i % 2) * 5, y0, 2, y1 - y0);
+    });
+    const runs = changeRows.map(([a, b]) => b - a);
+    const [mostCommonHeight] = mostCommon(runs);
+    if (!mostCommonHeight) return;
+    setScaleFactor(mostCommonHeight);
+  }, [originalCanvasRef.current, input]);
+  React.useEffect(() => {
+    const canvas = scaledCanvasRef.current;
+    if (!(input && canvas)) {
+      return;
+    }
+    const width = (canvas.width = input.image.width / scaleFactor);
+    const height = (canvas.height = input.image.height / scaleFactor);
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(input.image, 0, 0, width, height);
+  }, [scaledCanvasRef.current, input, scaleFactor]);
   return (
-    <div>
-      <label>
-        Pick image file:&nbsp;
-        <input type="file" accept="image/*" onChange={handleImageChange} />
-      </label>
-      <br />
-      <canvas ref={canvasRef} />
+    <div id="container">
+      <div>
+        <div className="help">
+          This app detects the scale factor of an upscaled pixel art image and
+          scales it down for you. You can right-click the result image to save
+          or copy it.
+          <br />
+          Implementation: <a href="https://twitter.com/akx">@akx</a>, algorithm:{" "}
+          <a href="https://twitter.com/ochrons">@ochrons</a>
+        </div>
+        <label>
+          Pick a pixel art image:&nbsp;
+          <input type="file" accept="image/*" onChange={handleImageChange} />
+        </label>
+      </div>
+      <div>Scale factor guess: {scaleFactor}&times;</div>
+      <canvas ref={originalCanvasRef} />
+      <canvas ref={scaledCanvasRef} style={{ imageRendering: "pixelated" }} />
     </div>
   );
 }
